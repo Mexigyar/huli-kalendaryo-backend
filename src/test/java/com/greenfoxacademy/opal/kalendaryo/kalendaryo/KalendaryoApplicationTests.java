6eac15ee40de600f735292ca73167cca28f563bb
bekobarna <bocce15@gmail.com>
2018-01-16 10:29:47 +0100

    auth test in progress

--- src/test/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplicationTests.java
+++ src/test/java/com/greenfoxacademy/opal/kalendaryo/kalendaryo/KalendaryoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.greenfoxacademy.opal.kalendaryo.kalendaryo;
 
-import com.greenfoxacademy.opal.kalendaryo.kalendaryo.model.UserModel;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import java.nio.charset.Charset;
 
@@ -25,11 +26,11 @@ import java.nio.charset.Charset;
 @EnableWebMvc
 public class KalendaryoApplicationTests {
 
-	private UserModel contentType;
+	//private UserModel contentType;
 
-	/*private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
 		MediaType.APPLICATION_JSON.getSubtype(),
-		Charset.forName("utf8"));*/
+		Charset.forName("utf8"));
 
 	@Autowired
 	WebApplicationContext webApplicationContext;
@@ -45,16 +46,21 @@ public class KalendaryoApplicationTests {
 	public void postAuthEndpointGivesBackUserModel() throws Exception {
 
 		JSONObject response = new JSONObject();
+		response.put("email", "huli.opal.kalendaryo@gmail.com");
+		response.put("authCode", "4/AAD6M1w0SblnW7knWD3V6aFyncYfMzzQQgapjIEKPfi78YkX3n40EeO03L2_I7KUn1kyxEaqjexNJNfQBXaR3GI");
+		response.put("displayName", "");
+		response.put("accessToken", "ya29.GlxEBSXkW7ErQ_JsoDfC3r6V7w0wdk4_cPQsVnFvjDvFN0vFruHC1gyngjqJSKNpp1of81_ziEkmKUzN-SZ8oMffeGZXPjMHF8inG6_-xuQMoBgqt5cjKWA9qkvt7Q");
+		response.put("refreshToken", "");
+		response.put("userId", "12");
+		response.put("calendarId", "");
 
-		response.put("kind", "api#channel");
-		response.put("id", "01234567-89ab-cdef-0123456788");
-		response.put("resourceId", "WDOXEjsdYtXzZHq93mDhG6dfTrg");
-		response.put("resourceUri", "https://www.googleapis.com/calendar/v3/calendars/huli.opal.kalendaryo@gmail.com/events?maxResults=250&alt=json");
-		response.put("expiration", "1516102799000");
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("X-Client-Token", "uih3/MUTrHq8KaZqhen2PKln680=");
+
 
 		mock.perform(post("/auth")
 			.contentType(MediaType.APPLICATION_JSON)
-			.content(response.toString()))
+			.headers(headers)).andDo(print())
 			.andExpect(content().contentType(String.valueOf(contentType)));
 
 	}
